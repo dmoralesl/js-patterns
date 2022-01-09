f8fcf0f9e52b85866c95cd1ed98f50f6984f17d7
David Morales <dmorales474@gmail.com>
2022-01-09 11:38:23 +0100

    Pub-Sub refactor

--- pubsub/pub.js
+++ pubsub/pub.js
@@ -1,25 +1,17 @@
 import { connect } from 'amqplib';
 
-export class Publisher {
-  constructor(queueName) {
-    this.queueName = queueName;
-  }
 
-  sendToQueue(message) {
-    connect('amqp://localhost').then((conn) => {
-        return conn.createChannel().then((ch) => {
-          var ok = ch.assertExchange(this.queueName, 'fanout', {durable: false})
-      
-      
-          return ok.then(() =>  {
-            ch.publish(this.queueName, '', Buffer.from(message));
-            console.log(" [x] Sent '%s'", message);
-            return ch.close();
-          });
-        }).finally(() => { conn.close(); });
-      }).catch(console.warn);
-    
-  }
+connect('amqp://localhost')
+  .then((conn) => {
+    return conn.createChannel().then((ch) => {
+      const ok = ch.assertExchange('TestQueue', 'fanout', {durable: false});
+      const message = 'This message is from the publisher';
   
-}
-
+      return ok.then(() =>  {
+        ch.publish('TestQueue', '', Buffer.from(message));
+        console.log(" [x] Sent '%s'", message);
+        return ch.close();
+      });
+    }).finally(() => { conn.close(); });
+  })
+  .catch(console.warn);
--- pubsub/sub.js
+++ pubsub/sub.js
@@ -1,34 +1,28 @@
 import { connect } from 'amqplib';
 
-export class Subscriber {
-    constructor(queueName) {
-        this.queueName = queueName;
-    }
-
-    async listen() {
-        return connect('amqp://localhost').then((conn) => {
-            process.once('SIGINT', () => { conn.close(); });
-            return conn.createChannel().then((ch) => {
-              var ok = ch.assertExchange(this.queueName, 'fanout', {durable: false});
-              ok = ok.then(() => {
-                return ch.assertQueue('', {exclusive: true});
-              });
-              ok = ok.then((qok) => {
-                return ch.bindQueue(qok.queue, this.queueName, '').then(() => {
-                  return qok.queue;
-                });
-              });
-              ok = ok.then((queue) => {
-                return ch.consume(queue, logMessage, {noAck: true});
-              });
-              return ok.then(() => {
-                console.log(` [*] Waiting for ${this.queueName}. To exit press CTRL+C`);
-              });
-          
-              function logMessage(msg) {
-                console.log(" [x] '%s'", msg.content.toString());
-              }
-            });
-          }).catch(console.warn);
-    }
-}
+connect('amqp://localhost')
+  .then((conn) => {
+    process.once('SIGINT', () => { conn.close(); });
+    return conn.createChannel().then((ch) => {
+      const ok = ch.assertExchange('TestQueue', 'fanout', {durable: false});
+      ok = ok.then(() => {
+        return ch.assertQueue('', {exclusive: true});
+      });
+      ok = ok.then((qok) => {
+        return ch.bindQueue(qok.queue, 'TestQueue', '').then(() => {
+          return qok.queue;
+        });
+      });
+      ok = ok.then((queue) => {
+        return ch.consume(queue, logMessage, {noAck: true});
+      });
+      return ok.then(() => {
+        console.log(` [*] Waiting for TestQueue. To exit press CTRL+C`);
+      });
+  
+      function logMessage(msg) {
+        console.log(" [x] '%s'", msg.content.toString());
+      }
+    });
+  })
+  .catch(console.warn);
